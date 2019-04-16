3fa3bda67bcdd48c083f85c952a1a5426ff85b43
Paulo Castro <paulo@balena.io>
2019-04-16 01:50:03 +0100

    Fix tar stream path handling under Windows and bump resin-bundle-resolve
    User-visible (ish) changes:
    - The bump of resin-bundle-resolve provides part of the requirements to
      allow the use of alternative template or arch-specific Dockerfile names,
      such as MyDockerfile.template or MyDockerfile.armv7hf.
    - Provides part of the fix for incorrect path handling of tar streams
      on Windows (forward-slash vs backslash path separators) which cause
      `balena push ipAddress` and `balena deploy` to fail on Windows.
    
    Resolves: #51
    Connects-to: https://github.com/balena-io/balena-cli/issues/1133
    Depends-on: https://github.com/balena-io-modules/resin-bundle-resolve/pull/36
    Change-type: minor
    Signed-off-by: Paulo Castro <paulo@balena.io>

--- lib/path-utils.ts
+++ lib/path-utils.ts
@@ -14,24 +14,61 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { normalize, relative } from 'path';
+import * as _ from 'lodash';
+import * as path from 'path';
+import { posix } from 'path';
 
 // Export all of node's path functions, so that users of this
 // module need only import this file
 export * from 'path';
 
+const nativeSepRE = new RegExp(_.escapeRegExp(path.sep), 'g');
+const nativeDotDotRE = new RegExp('^\\.\\.$|\\.\\.' + _.escapeRegExp(path.sep));
+
 /**
- * Given two paths, check whether the first contains the second
- * @param path1 The potentially containing path
- * @param path2 The potentially contained path
+ * Given two native (platform-dependent) paths, check whether the first
+ * contains the second
+ * @param path1 The potentially containing native path
+ * @param path2 The potentially contained native path
  * @return A boolean indicating whether `path1` contains `path2`
  */
 export const contains = (path1: string, path2: string): boolean => {
 	// First normalise the input, to remove any path weirdness
-	path1 = normalize(path1);
-	path2 = normalize(path2);
+	path1 = path.normalize(path1);
+	path2 = path.normalize(path2);
 
 	// Now test if any part of the relative path contains a .. ,
 	// which would tell us that path1 is not part of path2
-	return !/^\.\.$|\.\.\//.test(relative(path1, path2));
+	return !nativeDotDotRE.test(path.relative(path1, path2));
 };
+
+/**
+ * Given two POSIX paths, check whether the first contains the second
+ * @param path1 The potentially containing POSIX path
+ * @param path2 The potentially contained POSIX path
+ * @return A boolean indicating whether `path1` contains `path2`
+ */
+export const posixContains = (path1: string, path2: string): boolean => {
+	// First normalise the input, to remove any path weirdness
+	path1 = posix.normalize(path1);
+	path2 = posix.normalize(path2);
+
+	// Now test if any part of the relative path contains a .. ,
+	// which would tell us that path1 is not part of path2
+	return !/^\.\.$|\.\.\//.test(posix.relative(path1, path2));
+};
+
+/**
+ * Replace "native path separators" like '\' with the POSIX '/' separator.
+ * This function is handy but bear in mind that conversion from Windows paths
+ * to POSIX is not that simple when a drive letter is specified for either
+ * absolute or per-drive relative paths, e.g. 'C:\absolute' or 'C:relative',
+ * or when the path refers to a network server, e.g. '\\server\folder\file'.
+ */
+export function toPosixPath(p: string): string {
+	return p.replace(nativeSepRE, '/');
+}
+
+export function toNativePath(p: string): string {
+	return p.replace(/\//g, path.sep);
+}
